d87ed0f5891878ec368080662332c9862a9ccefa
Francis <francisdovnf@gmail.com>
2023-03-08 22:14:51 -0300

    nova att

--- sistema_frota_v1/Form2.Designer.cs
+++ sistema_frota_v1/Form2.Designer.cs
@@ -28,20 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.empresaPrefeituraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manutençãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suporteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordensDeServiçoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.credenciamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administradorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordensDeServiçoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.credenciamentoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.administradorToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.recuperarSenhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empresaPrefeituraToolStripMenuItem,
+            this.manutençãoToolStripMenuItem,
+            this.suporteToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(895, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked_1);
+            // 
+            // empresaPrefeituraToolStripMenuItem
+            // 
+            this.empresaPrefeituraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ordensDeServiçoToolStripMenuItem,
+            this.credenciamentoToolStripMenuItem,
+            this.administradorToolStripMenuItem});
+            this.empresaPrefeituraToolStripMenuItem.Name = "empresaPrefeituraToolStripMenuItem";
+            this.empresaPrefeituraToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.empresaPrefeituraToolStripMenuItem.Text = "Empresa/Prefeitura";
+            // 
+            // manutençãoToolStripMenuItem
+            // 
+            this.manutençãoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ordensDeServiçoToolStripMenuItem1,
+            this.credenciamentoToolStripMenuItem1,
+            this.administradorToolStripMenuItem1});
+            this.manutençãoToolStripMenuItem.Name = "manutençãoToolStripMenuItem";
+            this.manutençãoToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.manutençãoToolStripMenuItem.Text = "Manutenção";
+            // 
+            // suporteToolStripMenuItem
+            // 
+            this.suporteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recuperarSenhaToolStripMenuItem,
+            this.fedbackToolStripMenuItem});
+            this.suporteToolStripMenuItem.Name = "suporteToolStripMenuItem";
+            this.suporteToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.suporteToolStripMenuItem.Text = "Suporte";
+            // 
+            // ordensDeServiçoToolStripMenuItem
+            // 
+            this.ordensDeServiçoToolStripMenuItem.Name = "ordensDeServiçoToolStripMenuItem";
+            this.ordensDeServiçoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ordensDeServiçoToolStripMenuItem.Text = "usuarios";
+            // 
+            // credenciamentoToolStripMenuItem
+            // 
+            this.credenciamentoToolStripMenuItem.Name = "credenciamentoToolStripMenuItem";
+            this.credenciamentoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.credenciamentoToolStripMenuItem.Text = "frotas";
+            // 
+            // administradorToolStripMenuItem
+            // 
+            this.administradorToolStripMenuItem.Name = "administradorToolStripMenuItem";
+            this.administradorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.administradorToolStripMenuItem.Text = "empenhos";
+            // 
+            // ordensDeServiçoToolStripMenuItem1
+            // 
+            this.ordensDeServiçoToolStripMenuItem1.Name = "ordensDeServiçoToolStripMenuItem1";
+            this.ordensDeServiçoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.ordensDeServiçoToolStripMenuItem1.Text = "ordens de serviço";
+            // 
+            // credenciamentoToolStripMenuItem1
+            // 
+            this.credenciamentoToolStripMenuItem1.Name = "credenciamentoToolStripMenuItem1";
+            this.credenciamentoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.credenciamentoToolStripMenuItem1.Text = "credenciamento";
+            // 
+            // administradorToolStripMenuItem1
+            // 
+            this.administradorToolStripMenuItem1.Name = "administradorToolStripMenuItem1";
+            this.administradorToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.administradorToolStripMenuItem1.Text = "administrador";
+            // 
+            // recuperarSenhaToolStripMenuItem
+            // 
+            this.recuperarSenhaToolStripMenuItem.Name = "recuperarSenhaToolStripMenuItem";
+            this.recuperarSenhaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.recuperarSenhaToolStripMenuItem.Text = "recuperar  senha";
+            // 
+            // fedbackToolStripMenuItem
+            // 
+            this.fedbackToolStripMenuItem.Name = "fedbackToolStripMenuItem";
+            this.fedbackToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fedbackToolStripMenuItem.Text = "fedback";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(895, 427);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem empresaPrefeituraToolStripMenuItem;
+        private ToolStripMenuItem manutençãoToolStripMenuItem;
+        private ToolStripMenuItem suporteToolStripMenuItem;
+        private ToolStripMenuItem ordensDeServiçoToolStripMenuItem;
+        private ToolStripMenuItem credenciamentoToolStripMenuItem;
+        private ToolStripMenuItem administradorToolStripMenuItem;
+        private ToolStripMenuItem ordensDeServiçoToolStripMenuItem1;
+        private ToolStripMenuItem credenciamentoToolStripMenuItem1;
+        private ToolStripMenuItem administradorToolStripMenuItem1;
+        private ToolStripMenuItem recuperarSenhaToolStripMenuItem;
+        private ToolStripMenuItem fedbackToolStripMenuItem;
+        private PictureBox pictureBox1;
     }
 }
--- sistema_frota_v1/Form2.cs
+++ sistema_frota_v1/Form2.cs
@@ -16,5 +16,31 @@ namespace sistema_frota_v1
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
